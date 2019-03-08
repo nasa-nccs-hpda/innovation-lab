90903a57dc868709627d0c2cc08c1fcfb9d2b51b
a <a@gs619-rgillm1.local>
2019-03-08 14:50:01 -0500

    MaxEntRequest

--- model/ImageFile.py
+++ model/ImageFile.py
@@ -1,184 +1,41 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-from abc import ABCMeta
-import math
-import shutil
-import tempfile
-
 from osgeo import gdal
 from osgeo import gdalconst
-from osgeo.osr import SpatialReference
 
 from model.BaseFile import BaseFile
-from model.Envelope import Envelope
-from model.SystemCommand import SystemCommand
 
 
 # -----------------------------------------------------------------------------
 # class ImageFile
 #
-# We currently have one common image class, NC_File.  In case we use others,
-# image references can use this base Image class.  As new image classes are
-# derived from this, references will not need to be changed.
+# This class represents our single image format, NetCDF.
 # -----------------------------------------------------------------------------
 class ImageFile(BaseFile):
 
-    __metaclass__ = ABCMeta
-
-    BASE_GDAL_CMD = 'gdalwarp '
+    EXTENSION = '.nc'
     
     # -------------------------------------------------------------------------
     # __init__
     # -------------------------------------------------------------------------
-    def __init__(self, pathToFile, expectedExtension=None):
+    def __init__(self, pathToFile):
 
         # Initialize the base class.
-        super(ImageFile, self).__init__(pathToFile, expectedExtension)
-
-    # -------------------------------------------------------------------------
-    # clipReproject
-    #
-    # These three operations, clipping, reprojection and resampling can be
-    # combined into a single GDAL call.  This must be more efficient than
-    # invoking them individually.
-    #
-    # OutputFormat is the GDAL image format name, which defaults to GeoTiff.
-    # See https://gdal.org/formats_list.html.
-    # -------------------------------------------------------------------------
-    def clipReproject(self, envelope=None, outputSRS=None):
-
-        # At least one operation must be configured.
-        if not envelope and not outputSRS:
-
-            raise RuntimeError('Clip envelope or output SRS must be ' +
-                               'specified.')
-
-        # ---
-        # Configure the base command.  Specify the output format.  Otherwise,
-        # gdalwarp automatically converts to GeoTiff.
-        # ---
-        cmd = ImageFile.BASE_GDAL_CMD + ' -of ' + self.getFormatName()
-
-        # Clip?
-        if envelope:
-
-            if not isinstance(envelope, Envelope):
-                raise TypeError('The first parameter must be an Envelope.')
-
-            if not self.envelope().Intersection(envelope):
-
-                raise RuntimeError('The clip envelope does not intersect ' +
-                                   'the image.')
-                                   
-            cmd += (' -te' +
-                    ' ' + str(envelope.ulx()) +
-                    ' ' + str(envelope.lry()) +
-                    ' ' + str(envelope.lrx()) +
-                    ' ' + str(envelope.uly()) +
-                    ' -te_srs' +
-                    ' "' + envelope.GetSpatialReference().ExportToProj4() + 
-                    '"')
-
-        # Reproject?
-        if outputSRS and not self.srs().IsSame(outputSRS):
-            cmd += ' -t_srs "' + outputSRS.ExportToProj4() + '"'
-
-        # Finish the command.
-        outFile = tempfile.mkstemp()[1]
-        cmd += ' ' + self._filePath + ' ' + outFile
-        SystemCommand(cmd, None, True)
-        shutil.move(outFile, self._filePath)
-
-    # -------------------------------------------------------------------------
-    # envelope
-    # -------------------------------------------------------------------------
-    def envelope(self):
+        super(ImageFile, self).__init__(pathToFile, ImageFile.EXTENSION)
         
-        dataset = self._getDataset()
-        xform = dataset.GetGeoTransform()
-    	xScale = xform[1]
-    	yScale = xform[5]
-    	width  = dataset.RasterXSize
-    	height = dataset.RasterYSize
-    	ulx = xform[0]
-    	uly = xform[3]	
-    	lrx = ulx + width  * xScale
-    	lry = uly + height * yScale
-
-        srs = self.srs()
-        envelope = Envelope()
-        envelope.addPoint(ulx, uly, 0, srs)
-        envelope.addPoint(lrx, lry, 0, srs)
-
-        return envelope
+        self._dataset = None
         
     # -------------------------------------------------------------------------
     # _getDataset
     # -------------------------------------------------------------------------
     def _getDataset(self):
 
-        dataset = gdal.Open(self._filePath, gdalconst.GA_ReadOnly)
+        if not self._dataset:
+            
+            self._dataset = gdal.Open(self._filePath, gdalconst.GA_ReadOnly)
 
-        if not dataset:
-            raise RuntimeError('Unable to read ' + self._filePath + '.')
+            if not self._dataset:
+                raise RuntimeError('Unable to read ' + self._filePath + '.')
 
-        return dataset
-
-    # -------------------------------------------------------------------------
-    # getFormatName
-    # -------------------------------------------------------------------------
-    def getFormatName(self):
-
-        ds = self._getDataset()
-        return ds.GetDriver().ShortName
-
-    # -------------------------------------------------------------------------
-    # getSquareScale
-    #
-    # Some ASCII image variants cannot represent pixel size in two dimensions.
-    # When an ASCII image with rectangular pixels is represented with a
-    # single-dimension variant, it introduces a ground shift.  This method
-    # returns a single value suitable for resampling the pixels to be square.
-    # The image must be resampled using this value.
-    # -------------------------------------------------------------------------
-    def getSquareScale(self):
-
-        xScale = self.scale()[0]
-        yScale = self.scale()[1]
-
-        if math.fabs(xScale) > math.fabs(yScale):
-            return math.fabs(xScale * -1)  # xScale bigger, so increase yScale
-
-        return math.fabs(yScale * -1)
-
-    # -------------------------------------------------------------------------
-    # resample
-    # -------------------------------------------------------------------------
-    def resample(self, xScale, yScale):
-        
-        cmd = ImageFile.BASE_GDAL_CMD + ' -tr ' + str(xScale) + ' ' + \
-              str(yScale)
-
-        # Finish the command.
-        outFile = tempfile.mkstemp()[1]
-        cmd += ' ' + self._filePath + ' ' + outFile
-        SystemCommand(cmd, None, True)
-        shutil.move(outFile, self._filePath)
-        
-    # -------------------------------------------------------------------------
-    # scale
-    # -------------------------------------------------------------------------
-    def scale(self):
-
-        xform = self._getDataset().GetGeoTransform()
-        return xform[1], xform[5]
-
-    # -------------------------------------------------------------------------
-    # srs
-    # -------------------------------------------------------------------------
-    def srs(self):
-
-        outSrs = SpatialReference()
-        outSrs.ImportFromWkt(self._getDataset().GetProjection())
-        return outSrs
+        return self._dataset
--- model/tests/test_ImageFile.py
+++ model/tests/test_ImageFile.py
@@ -2,15 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os
-import shutil
-import tempfile
 import unittest
 
-from osgeo import gdal
-from osgeo import gdalconst
-from osgeo.osr import SpatialReference
-
-from model.Envelope import Envelope
 from model.ImageFile import ImageFile
 
 
@@ -23,279 +16,30 @@ from model.ImageFile import ImageFile
 class ImageFileTestCase(unittest.TestCase):
 
     TEST_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                             'gsenm_250m_eucl_dist_streams.tif')
-
-    # -------------------------------------------------------------------------
-    # testClipReproject
-    # -------------------------------------------------------------------------
-    def testClipReproject(self):
-
-        # Build the test file.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy, '.tif')
-
-        # Build the envelope.
-        ulx = 367080
-        uly = 4209230
-        lrx = 509200
-        lry = 4095100
-        srs = SpatialReference()
-        srs.ImportFromEPSG(32612)
-        env = Envelope()
-        env.addPoint(ulx, uly, 0, srs)
-        env.addPoint(lrx, lry, 0, srs)
-
-        # Reprojection parameter
-        targetSRS = SpatialReference()
-        targetSRS.ImportFromEPSG(4326)
-
-        # Clip, reproject and resample.
-        imageFile.clipReproject(env, targetSRS,)
-
-        # Check the results.
-        dataset = gdal.Open(workingCopy, gdalconst.GA_ReadOnly)
-
-        if not dataset:
-            raise RuntimeError('Unable to read ' + workingCopy + '.')
-
-        xform = dataset.GetGeoTransform()
-        xScale = xform[1]
-        yScale = xform[5]
-        width = dataset.RasterXSize
-        height = dataset.RasterYSize
-        clippedUlx = xform[0]
-        clippedUly = xform[3]
-        clippedLrx = clippedUlx + width * xScale
-        clippedLry = clippedUly + height * yScale
-
-        self.assertAlmostEqual(clippedUlx, -112.49369402670872, places=12)
-        self.assertAlmostEqual(clippedUly, 38.03073206024332, places=11)
-        self.assertAlmostEqual(clippedLrx, -110.89516946364738, places=12)
-        self.assertAlmostEqual(clippedLry, 36.99265291293727, places=11)
-
-        outSRS = SpatialReference()
-        outSRS.ImportFromWkt(dataset.GetProjection())
-        self.assertTrue(outSRS.IsSame(targetSRS))
-
-        # Delete the test file.
-        os.remove(workingCopy)
-
-    # -------------------------------------------------------------------------
-    # testClip
-    # -------------------------------------------------------------------------
-    def testClip(self):
-
-        # Build the test file.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy)
-
-        # Build the envelope and clip.
-        ulx = 367080
-        uly = 4209230
-        lrx = 509200
-        lry = 4095100
-        srs = SpatialReference()
-        srs.ImportFromEPSG(32612)
-        env = Envelope()
-        env.addPoint(ulx, uly, 0, srs)
-        env.addPoint(lrx, lry, 0, srs)
-        imageFile.clipReproject(env)
-
-        # Check the corners.
-        dataset = gdal.Open(workingCopy, gdalconst.GA_ReadOnly)
-
-        if not dataset:
-            raise RuntimeError('Unable to read ' + workingCopy + '.')
-
-        xform = dataset.GetGeoTransform()
-        xScale = xform[1]
-        yScale = xform[5]
-        width = dataset.RasterXSize
-        height = dataset.RasterYSize
-        clippedUlx = xform[0]
-        clippedUly = xform[3]
-        clippedLrx = ulx + width * xScale
-        clippedLry = uly + height * yScale
-
-        self.assertEqual(clippedUlx, ulx)
-        self.assertEqual(clippedUly, uly)
-        self.assertEqual(clippedLrx, lrx)
-        self.assertEqual(clippedLry, lry)
-
-        # Delete the test file.
-        os.remove(workingCopy)
-
-    # -------------------------------------------------------------------------
-    # testEnvelope
-    # -------------------------------------------------------------------------
-    def testEnvelope(self):
-        
-        # Create a test image.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy)
-
-        # Test envelope.
-        srs = SpatialReference()
-        srs.ImportFromEPSG(32612)
-        expectedEnvelope = Envelope()
-        expectedEnvelope.addPoint(367073.627681211, 4209234.221027355, 0, srs)
-        expectedEnvelope.addPoint(509261.627681211, 4095090.2210273547, 0, srs)
-        
-        envelope = imageFile.envelope()
-        self.assertTrue(imageFile.envelope().Equals(expectedEnvelope))
-
-        # Delete the test file.
-        os.remove(workingCopy)
-        
-        
-    # -------------------------------------------------------------------------
-    # testGetFormatName
-    # -------------------------------------------------------------------------
-    def testGetFormatName(self):
-
-        # Create a test image.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy)
-
-        # Test getFormatName.
-        self.assertEqual('GTiff', imageFile.getFormatName())
-
-        # Delete the test file.
-        os.remove(workingCopy)
-
+                             'TSURF.nc')
+    
     # -------------------------------------------------------------------------
     # test_getDataset
     # -------------------------------------------------------------------------
     def test_getDataset(self):
 
         # Create a test image.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy)
+        imageFile = ImageFile(ImageFileTestCase.TEST_FILE)
 
         # Test _getDataset.
         self.assertIsNotNone(imageFile._getDataset())
 
         # Delete the test file.
-        os.remove(workingCopy)
+        os.remove(imageFile.fileName())
 
     # -------------------------------------------------------------------------
-    # testGetSquareScale
+    # testInvalidImageType
     # -------------------------------------------------------------------------
-    def testGetSquareScale(self):
-
-        img = ImageFile(ImageFileTestCase.TEST_FILE)
-        self.assertEqual(img.getSquareScale(), 246.0)
-
-    # -------------------------------------------------------------------------
-    # testNoOperation
-    # -------------------------------------------------------------------------
-    def testNoOperation(self):
-
-        # Create a test image.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy)
-
-        # Test with no operation specified.
-        with self.assertRaisesRegexp(RuntimeError, 'envelope or output SRS'):
-            imageFile.clipReproject()
-
-        # Delete the test file.
-        os.remove(workingCopy)
-
-    # -------------------------------------------------------------------------
-    # testReproject
-    # -------------------------------------------------------------------------
-    def testReproject(self):
-
-        # Build the test file.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy)
-
-        # Reproject.
-        targetSRS = SpatialReference()
-        targetSRS.ImportFromEPSG(4326)
-        imageFile.clipReproject(outputSRS=targetSRS)
-
-        # Check the SRS.
-        dataset = gdal.Open(workingCopy, gdalconst.GA_ReadOnly)
-
-        if not dataset:
-            raise RuntimeError('Unable to read ' + workingCopy + '.')
-
-        outSRS = SpatialReference()
-        outSRS.ImportFromWkt(dataset.GetProjection())
-        self.assertTrue(outSRS.IsSame(targetSRS))
-
-        # Delete the test file.
-        os.remove(workingCopy)
-
-    # -------------------------------------------------------------------------
-    # testResample
-    # -------------------------------------------------------------------------
-    def testResample(self):
-
-        # Build the test file.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy)
-
-        # Scale.  Original scale is (246, -246).
-        targetX_Scale = 100
-        targetY_Scale = -150
-
-        imageFile.resample(targetX_Scale, targetY_Scale)
-
-        # Check the scale.
-        dataset = gdal.Open(workingCopy, gdalconst.GA_ReadOnly)
-
-        if not dataset:
-            raise RuntimeError('Unable to read ' + workingCopy + '.')
-
-        xform = dataset.GetGeoTransform()
-        self.assertEqual(xform[1], targetX_Scale)
-        self.assertEqual(xform[5], targetY_Scale)
-
-        # Delete the test file.
-        os.remove(workingCopy)
-
-    # -------------------------------------------------------------------------
-    # testScale
-    # -------------------------------------------------------------------------
-    def testScale(self):
-
-        # Build the test file.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy)
-
-        # Check the scale.
-        self.assertEqual(imageFile.scale()[0], 246)
-        self.assertEqual(imageFile.scale()[1], -246)
-
-        # Delete the test file.
-        os.remove(workingCopy)
-
-    # -------------------------------------------------------------------------
-    # testSrs
-    # -------------------------------------------------------------------------
-    def testSrs(self):
-
-        # Build the test file.
-        workingCopy = tempfile.mkstemp(suffix='.tif')[1]
-        shutil.copyfile(ImageFileTestCase.TEST_FILE, workingCopy)
-        imageFile = ImageFile(workingCopy)
-
-        # Check the srs.
-        expectedSRS = SpatialReference()
-        expectedSRS.ImportFromEPSG(32612)
-        self.assertTrue(imageFile.srs().IsSame(expectedSRS))
-
-        # Delete the test file.
-        os.remove(workingCopy)
+    def testInvalidImageType(self):
+        
+        testFile = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'invalid.tif')
+        
+        with self.assertRaisesRegexp(RuntimeError, 'is not in \.nc'):
+            ImageFile(testFile)
+    
