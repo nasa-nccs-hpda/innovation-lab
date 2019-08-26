498edb621bd34d292c50fa1337c0658f79b785ec
jli30 <jian.li@nasa.gov>
2019-08-26 17:24:37 -0400

    Error handling for CLI parameters

--- view/MmxRequestCommandLineView.py
+++ view/MmxRequestCommandLineView.py
@@ -56,31 +56,52 @@ def main():
     parser.add_argument('-n',
                         default=10,
                         help='Number of trials to run')
-                        
+
+    parser.add_argument('-k',
+                        default=10,
+                        type=int,
+                        help='Number of predictors for each trial')
+
     parser.add_argument('-o',
                         default='.',
                         help='Path to output directory')
 
+    parser.add_argument('-i',
+                        default=None,
+                        help='Path to input directory')
+
     parser.add_argument('--source',
-                        default='.',
-                        help='<Optional> Source of input data (e.g., Edas, Mas)')
+                        default='Edas',
+                        help='Data source')
+
+    parser.add_argument('--workflow',
+                        default='',
+                        help='Type of analysis')
 
     args = parser.parse_args()
 
     # prepare context - convert CLI parameters to context-sensitive dictionary
     context = {}
-    context['outDir'] = args.o
-    context['numTrials'] = args.n
-    context['observationFilePath'] = args.f
-    context['species'] = args.s
-    context['startDate'] = args.start_date
-    context['endDate'] = args.end_date
-    context['collection'] = args.c
-    context['listOfVariables'] = args.vars
-    context['operation'] = args.opr
-    context['source'] = args.source
+    if args.o is not None: context['outDir'] = args.o
+    if args.i is not None: context['inDir'] = args.i
+    if args.n is not None: context['numTrials'] = args.n
+    if args.f is not None: context['observationFilePath'] = args.f
+    if args.s is not None: context['species'] = args.s
+    if args.start_date is not None: context['startDate'] = args.start_date
+    if args.end_date is not None: context['endDate'] = args.end_date
+    if args.c is not None: context['collection'] = args.c
+    if args.vars is not None: context['listOfVariables'] = args.vars
+    if args.opr is not None: context['operation'] = args.opr
+    if args.k is not None: context['numPredictors'] = args.k
+    if args.source is not None: context['source'] = args.source
+    if args.workflow is not '': context['workflow'] = args.workflow
 
-    mmxr = MmxRequest(context)
+    # Dynamically select workflow request (defaults to MmxRequest)
+    clazzName = "Mmx{0}Request".format(args.workflow)
+    # import the class from module
+    mod = __import__("model.{0}".format(clazzName), globals(), locals(), [clazzName])
+    requestInstance = getattr(mod, clazzName)
+    mmxr = requestInstance(context, context['source'])
     mmxr.run()
 
 # ------------------------------------------------------------------------------
