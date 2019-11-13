49b70aa88107be9c2a0c64385068a006817bab02
Roger Gill <>
2019-11-13 09:34:35 -0500

    aviris-serial

--- projects/aviris_regression_algorithms/model/ApplyAlgorithm.py
+++ projects/aviris_regression_algorithms/model/ApplyAlgorithm.py
@@ -104,9 +104,7 @@ class ApplyAlgorithm(object):
                     floatValue = struct.unpack('f', bandValue)[0]
                     print 'fv = ' + str(floatValue)
 
-                    import pdb
-                    pdb.set_trace()
-                    P += bandValue * float(coef[algorithmName])
+                    P += floatValue * float(coef[algorithmName])
 
                 outDs.WriteRaster(col, row, 1, 1, P)
                 
