d48132983418635126b870ce78f91cf0ce53124f
Roger Gill <>
2019-11-13 10:35:11 -0500

    aviris-serial

--- projects/aviris_regression_algorithms/model/tests/test_ApplyAlgorithm.py
+++ projects/aviris_regression_algorithms/model/tests/test_ApplyAlgorithm.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+import logging
 import os
+import sys
 import tempfile
 import unittest
 
 from projects.aviris_regression_algorithms.model.ApplyAlgorithm \
     import ApplyAlgorithm
 
+
+logger = logging.getLogger()
+logger.level = logging.DEBUG
 
 # -----------------------------------------------------------------------------
 # class ApplyAlgorithmTestCase
@@ -24,6 +29,9 @@ class ApplyAlgorithmTestCase(unittest.TestCase):
     # test
     # -------------------------------------------------------------------------
     def test(self):
+
+        streamHandler = logging.StreamHandler(sys.stdout)
+        logger.addHandler(streamHandler)
 
         coefFile = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 'Chl_Coeff_input.csv')
