02d4b6492a8ef32921d7f7172240bfd516f16e06
Roger Gill <>
2019-11-14 10:30:17 -0500

    aviris-serial

--- projects/aviris_regression_algorithms/model/ApplyAlgorithm.py
+++ projects/aviris_regression_algorithms/model/ApplyAlgorithm.py
@@ -90,6 +90,8 @@ class ApplyAlgorithm(object):
                 # and coefficient information.  
                 # {bandNumber: (coefficient, pixel value)}
                 # ---
+                import pdb
+                pdb.set_trace()
                 bandCoefValueDict = \
                     self.associateValuesWithCoefs(pixelStack, algorithmName)
                 
@@ -166,56 +168,6 @@ class ApplyAlgorithm(object):
         return math.sqrt(tally)
         
     # -------------------------------------------------------------------------
-    # isMaskedOrNoData
-    # -------------------------------------------------------------------------
-    # def isMaskedOrNoData(self, col, row):
-    #
-    #     # ---
-    #     # Mask: band 10 > 0.8
-    #     # Also check for a no-data value.  When found the output value is
-    #     # no data.
-    #     # ---
-    #     bandValue = self.readOnePixelToFloat(col, row, 9)
-    #
-    #     if bandValue > 0.8 or bandValue == ApplyAlgorithm.NO_DATA_VALUE:
-    #         return True
-    #
-    #     # Mask: band 426 < 0.01
-    #     bandValue = self.readOnePixelToFloat(col, row, 425)
-    #     if bandValue < 0.01: return True
-    #
-    #     return False
-        
-    # -------------------------------------------------------------------------
-    # readOnePixelToFloat
-    # -------------------------------------------------------------------------
-    # def readOnePixelToFloat(self, col, row, band):
-    #
-    #     pixelAsString = self. \
-    #                     imageFile. \
-    #                     _getDataset(). \
-    #                     ReadRaster(col,
-    #                                row,
-    #                                1, # x read size
-    #                                1, # y read size
-    #                                1, # buf_xsize
-    #                                1, # buf_ysize
-    #                                None, # buf_type read from image
-    #                                [band])
-    #
-    #     pixelAsFloat = struct.unpack('f', pixelAsString)[0]
-    #
-    #     if self.logger:
-    #
-    #         msg = '(band, value) = (' + \
-    #               str(band) + ', ' + \
-    #               str(pixelAsFloat) + ')'
-    #
-    #         self.logger.info(msg)
-    #
-    #     return pixelAsFloat
-        
-    # -------------------------------------------------------------------------
     # readStack
     # -------------------------------------------------------------------------
     def readStack(self, col, row):
@@ -227,83 +179,3 @@ class ApplyAlgorithm(object):
 
         return pixelsAsFloats
         
-    # -------------------------------------------------------------------------
-    # applyAlgorithm
-    #
-    # P = a0 + a1b1 + a2b2 …
-    # -------------------------------------------------------------------------
-    # def applyAlgorithmOrig(self, algorithmName):
-    #
-    #     # Ensure the algorithm name is valid.
-    #     if algorithmName not in self.coefs[0]:
-    #
-    #         raise RuntimeError('Algorithm ' + \
-    #                            str(algorithmName) + \
-    #                            ' not in coeffient file, ' + \
-    #                            self.coefFile)
-    #
-    #     # Create the output raster.
-    #     outName = os.path.join(self.outDir, algorithmName + '.tif')
-    #     driver = gdal.GetDriverByName('GTiff')
-    #
-    #     outDs = driver.Create(outName,
-    #                           self.imageFile._getDataset().RasterXSize,
-    #                           self.imageFile._getDataset().RasterYSize)
-    #
-    #     # ---
-    #     # Remove the coefficients that are zero.  Only the bands for non-zero
-    #     # terms will be extracted.
-    #     # ---
-    #     coefs = [coef for coef in self.coefs if coef[algorithmName] != 0]
-    #
-    #     # The first term is the y intercept.
-    #     P = float(self.coefs[0][algorithmName])
-    #
-    #     # ---
-    #     # Iterate through the raster, extracting the pixel for each non-zero
-    #     # band, and computing the output pixel value.
-    #     # ---
-    #     for row in range(self.imageFile._getDataset().RasterYSize):
-    #         for col in range(self.imageFile._getDataset().RasterXSize):
-    #             for coefRow in coefs[1:]:
-    #
-    #                 coef = float(coefRow[algorithmName])
-    #
-    #                 if coef != 0:
-    #
-    #                     bandIndex = \
-    #                         int(re.search(r'\d{0,3}$',
-    #                                       coefRow['Band Number']).group())
-    #
-    #                     pixelStrValue = \
-    #                         self. \
-    #                         imageFile. \
-    #                         _getDataset(). \
-    #                         ReadRaster(col,
-    #                                    row,
-    #                                    1, # x read size
-    #                                    1, # y read size
-    #                                    1, # buf_xsize
-    #                                    1, # buf_ysize
-    #                                    None, # buf_type read from image
-    #                                    [bandIndex])
-    #
-    #                     pixelValue = struct.unpack('f', pixelStrValue)[0]
-    #
-    #                     if pixelValue != ApplyAlgorithm.NO_DATA_VALUE:
-    #                         P += pixelValue * coef
-    #
-    #                     if self.logger:
-    #
-    #                         msg = '(band, value, P) = (' + \
-    #                               str(bandIndex) + ', ' + \
-    #                               str(floatValue) + ', ' + \
-    #                               str(P) + ')'
-    #
-    #                         self.logger.info(msg)
-    #
-    #             hexValue = struct.pack('f', P)
-    #             outDs.WriteRaster(col, row, 1, 1, hexValue)
-    #
-    #     outDs.close()
-    #
