d6a35c2d7b18d9324f2111384b32429a71742979
Roger Gill <>
2019-11-15 12:43:50 -0500

    aviris-serial

--- projects/aviris_regression_algorithms/model/tests/test_ApplyAlgorithm.py
+++ projects/aviris_regression_algorithms/model/tests/test_ApplyAlgorithm.py
@@ -21,10 +21,6 @@ logger.level = logging.DEBUG
 # -----------------------------------------------------------------------------
 class ApplyAlgorithmTestCase(unittest.TestCase):
 
-    TEST_FILE = '/att/pubrepo/ORNL/ABoVE_Archive/daac.ornl.gov/daacdata/' \
-                'above/ABoVE_Airborne_AVIRIS_NG/data/' \
-                'ang20170709t224839_rfl_v2p9/ang20170709t224839_corr_v2p9_img'
-    
     # -------------------------------------------------------------------------
     # test
     # -------------------------------------------------------------------------
@@ -36,9 +32,8 @@ class ApplyAlgorithmTestCase(unittest.TestCase):
         coefFile = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 'Chl_Coeff_input.csv')
 
-        aa = ApplyAlgorithm(coefFile, 
-                            ApplyAlgorithmTestCase.TEST_FILE, 
-                            tempfile.gettempdir(),
-                            logger)
-                            
+        testImage = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                 'clip.img')
+
+        aa = ApplyAlgorithm(coefFile, testImage, tempfile.gettempdir(),logger)
         aa.applyAlgorithm('Avg Chl')
