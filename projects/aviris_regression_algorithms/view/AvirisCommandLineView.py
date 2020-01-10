2e4b0ce21ab9256ede26b9ea07ea3e104c5e059c
Roger Gill <>
2020-01-10 10:29:57 -0500

    aviris2

--- projects/aviris_regression_algorithms/view/AvirisCommandLineView.py
+++ projects/aviris_regression_algorithms/view/AvirisCommandLineView.py
@@ -21,7 +21,7 @@ from projects.aviris_regression_algorithms.model.ApplyAlgorithm \
 # projects/aviris_regression_algorithms/view/AvirisCommandLineView.py -a 'Avg Chl' -c /att/nobackup/rlgill/AVIRIS/Chl_Coeff_input.csv -i /att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG/data/ang20180819t010027/ang20180819t010027_rdn_v2r2/ang20180819t010027_rdn_v2r2_img -o /att/nobackup/rlgill/AVIRIS/test/output
 #
 # To screen
-# projects/aviris_regression_algorithms/view/AvirisCommandLineView.py -a 'Avg Chl' -c /att/nobackup/rlgill/AVIRIS/Chl_Coeff_input.csv -i /att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG/data/ang20180819t010027/ang20180819t010027_rdn_v2r2/ang20180819t010027_rdn_v2r2_img -o /att/nobackup/rlgill/AVIRIS/test/output
+# projects/aviris_regression_algorithms/view/AvirisCommandLineView.py -a 'Avg Chl' -c /att/nobackup/rlgill/AVIRIS/Chl_Coeff_input.csv -i /att/pubrepo/ABoVE/archived_data/ORNL/ABoVE_Airborne_AVIRIS_NG/data/ang20180819t010027/ang20180819t010027_rdn_v2r2/ang20180819t010027_rdn_v2r2_img -s
 # -----------------------------------------------------------------------------
 def main():
 
@@ -49,7 +49,7 @@ def main():
 
     group.add_argument('-s',
                        nargs='?',
-                       default=0.1,
+                       default=0.0,
                        help='Screen the image to determine if it has ' + 
                             'pixels that are not masked and not ' +
                             'no-data valued.  The value for this argument ' +
@@ -61,7 +61,9 @@ def main():
     args = parser.parse_args()
     aa = ApplyAlgorithm(args.c, args.i, args.o)
 
-    if args.s:
+    import pdb
+    pdb.set_trace()
+    if args.s > 0:
         aa.screen(args.s)
         
     else:
