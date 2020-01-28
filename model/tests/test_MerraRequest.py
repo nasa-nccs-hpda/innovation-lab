0538f55116926d3afd10d8fe4ce0fa990584a7b8
Roger Gill <>
2020-01-28 15:34:34 -0500

    merra-request

---
+++ model/tests/test_MerraRequest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import pandas
+
+from model.MerraRequest import MerraRequest
+
+
+# -----------------------------------------------------------------------------
+# class MerraRequestTestCase
+#
+# python -m unittest discover model/tests/
+# python -m unittest model.tests.test_MerraRequest
+# -----------------------------------------------------------------------------
+class MerraRequestTestCase(unittest.TestCase):
+
+    # -------------------------------------------------------------------------
+    # testQueryFiles
+    # -------------------------------------------------------------------------
+    def testQueryFiles(self):
+
+        import pdb
+        pdb.set_trace()
+        dateRange = pandas.date_range('2010-10-10', '2012-12-12')
+        files = MerraRequest.requestFiles(dateRange, 'tavgM_2d_slv_Nx')
