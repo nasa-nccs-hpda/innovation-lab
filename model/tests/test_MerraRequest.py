9028a59ff5e0a247cdd487dc093b863bad4e7e29
R <R>
2020-03-05 11:11:17 -0500

    merra-request

--- model/tests/test_MerraRequest.py
+++ model/tests/test_MerraRequest.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
+import tempfile
 import unittest
 
 import pandas
 
+from osgeo.osr import SpatialReference
+
+from model.Envelope import Envelope
 from model.MerraRequest import MerraRequest
 
 
@@ -15,44 +19,169 @@ from model.MerraRequest import MerraRequest
 # python -m unittest model.tests.test_MerraRequest
 # -----------------------------------------------------------------------------
 class MerraRequestTestCase(unittest.TestCase):
-
+    
     # -------------------------------------------------------------------------
     # testQueryFiles
     # -------------------------------------------------------------------------
     def testQueryFiles(self):
-
-        dateRange = pandas.date_range('2010-10-10', '2012-12-12')
-        files = MerraRequest.queryFiles(dateRange, 'tavgM_2d_slv_Nx')
         
-        self.assertEqual(27, len(files))
+        existingFiles, missingFiles = \
+            MerraRequest.queryFiles(\
+                pandas.date_range('2010-10-10', '2011-02-12'),
+                                  MerraRequest.MONTHLY,
+                                  MerraRequest.COLLECTIONS,
+                                  MerraRequest.OPERATIONS)
+        
+        expectedExisting = \
+         ['/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_avg_2010_month10.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_avg_2010_month11.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_avg_2010_month12.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_avg_2011_month01.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_avg_2011_month02.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_max_2010_month10.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_max_2010_month11.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_max_2010_month12.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_max_2011_month01.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_max_2011_month02.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_min_2010_month10.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_min_2010_month11.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_min_2010_month12.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_min_2011_month01.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_min_2011_month02.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_sum_2010_month10.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_sum_2010_month11.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_sum_2010_month12.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_sum_2011_month01.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxflx_sum_2011_month02.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_avg_2010_month10.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_avg_2010_month11.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_avg_2010_month12.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_avg_2011_month01.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_avg_2011_month02.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_max_2010_month10.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_max_2010_month11.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_max_2010_month12.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_max_2011_month01.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_max_2011_month02.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_min_2010_month10.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_min_2010_month11.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_min_2010_month12.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_min_2011_month01.nc',
+         '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_min_2011_month02.nc']
+        
+        self.assertEqual(expectedExisting, existingFiles)
+        
+        expectedMissing = \
+          ['/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_sum_2010_month10.nc',
+          '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_sum_2010_month11.nc',
+          '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_sum_2010_month12.nc',
+          '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_sum_2011_month01.nc',
+          '/att/pubrepo/ILAB/data/MERRA2/Monthly/m2t1nxslv_sum_2011_month02.nc']
+        
+        self.assertEqual(expectedMissing, missingFiles)
+    
+    # -------------------------------------------------------------------------
+    # testAdjustFrequency
+    #
+    # MerraRequest._adjustFrequency, as a private method, need not be included
+    # in the unit test.  This test is helpful at development time, and it won't
+    # hurt anything to remain a unit test.
+    # -------------------------------------------------------------------------
+    def testAdjustFrequency(self):
+        
+        # Request an invalid frequency.
+        dateRange = pandas.date_range('2010-10-10', '2012-12-12')
+        
+        with self.assertRaises(RuntimeError):
+            MerraRequest._adjustFrequency(dateRange, 'fortnight')
+        
+        # Request monthly.
+        monthlyRange = MerraRequest._adjustFrequency(dateRange,
+                                                     MerraRequest.MONTHLY)
+        
+        self.assertEqual(27, len(monthlyRange))
+        
+        expected = ['2010_month10', '2010_month11', '2010_month12',
+                    '2011_month01', '2011_month02', '2011_month03',
+                    '2011_month04', '2011_month05', '2011_month06',
+                    '2011_month07', '2011_month08', '2011_month09',
+                    '2011_month10', '2011_month11', '2011_month12',
+                    '2012_month01', '2012_month02', '2012_month03',
+                    '2012_month04', '2012_month05', '2012_month06',
+                    '2012_month07', '2012_month08', '2012_month09',
+                    '2012_month10', '2012_month11', '2012_month12']
+        
+        self.assertEqual(expected, monthlyRange)
+        
+        # Request weekly.
+        weeklyRange = MerraRequest._adjustFrequency(dateRange,
+                                                    MerraRequest.WEEKLY)
+        
+        self.assertEqual(117, len(weeklyRange))
+        
+        expected = ['2010_week40', '2010_week41', '2010_week42',
+                    '2010_week43', '2010_week44', '2010_week45',
+                    '2010_week46', '2010_week47', '2010_week48',
+                    '2010_week49', '2010_week50', '2010_week51',
+                    '2011_week52', '2011_week01', '2011_week02',
+                    '2011_week03', '2011_week04', '2011_week05',
+                    '2011_week06', '2011_week07', '2011_week08',
+                    '2011_week09', '2011_week10', '2011_week11',
+                    '2011_week12', '2011_week13', '2011_week14',
+                    '2011_week15', '2011_week16', '2011_week17',
+                    '2011_week18', '2011_week19', '2011_week20',
+                    '2011_week21', '2011_week22', '2011_week23',
+                    '2011_week24', '2011_week25', '2011_week26',
+                    '2011_week27', '2011_week28', '2011_week29',
+                    '2011_week30', '2011_week31', '2011_week32',
+                    '2011_week33', '2011_week34', '2011_week35',
+                    '2011_week36', '2011_week37', '2011_week38',
+                    '2011_week39', '2011_week40', '2011_week41',
+                    '2011_week42', '2011_week43', '2011_week44',
+                    '2011_week45', '2011_week46', '2011_week47',
+                    '2011_week48', '2011_week49', '2011_week50',
+                    '2011_week51', '2012_week52', '2012_week01',
+                    '2012_week02', '2012_week03', '2012_week04',
+                    '2012_week05', '2012_week06', '2012_week07',
+                    '2012_week08', '2012_week09', '2012_week10',
+                    '2012_week11', '2012_week12', '2012_week13',
+                    '2012_week14', '2012_week15', '2012_week16',
+                    '2012_week17', '2012_week18', '2012_week19',
+                    '2012_week20', '2012_week21', '2012_week22',
+                    '2012_week23', '2012_week24', '2012_week25',
+                    '2012_week26', '2012_week27', '2012_week28',
+                    '2012_week29', '2012_week30', '2012_week31',
+                    '2012_week32', '2012_week33', '2012_week34',
+                    '2012_week35', '2012_week36', '2012_week37',
+                    '2012_week38', '2012_week39', '2012_week40',
+                    '2012_week41', '2012_week42', '2012_week43',
+                    '2012_week44', '2012_week45', '2012_week46',
+                    '2012_week47', '2012_week48', '2012_week49',
+                    '2012_week50', '2012_week51', '2012_week52']
+        
+        self.assertEqual(expected, weeklyRange)
+    
+    # -------------------------------------------------------------------------
+    # testRun
+    # -------------------------------------------------------------------------
+    def testRun(self):
+       
+        ulx = -71
+        uly = 40
+        lrx = -70
+        lry = 39
+        srs = SpatialReference()
+        srs.ImportFromEPSG(4326)
+        env = Envelope()
+        env.addPoint(ulx, uly, 0, srs)
+        env.addPoint(lrx, lry, 0, srs)
 
-        expectedFiles = ['/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2010/MERRA2_300.tavgM_2d_slv_Nx.201010.nc4',
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2010/MERRA2_300.tavgM_2d_slv_Nx.201011.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2010/MERRA2_300.tavgM_2d_slv_Nx.201012.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201101.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201102.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201103.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201104.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201105.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201106.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201107.nc4',
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201108.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201109.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201110.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201111.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2011/MERRA2_400.tavgM_2d_slv_Nx.201112.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201201.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201202.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201203.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201204.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201205.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201206.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201207.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201208.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201209.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201210.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201211.nc4', 
-                         '/att/pubrepo/ILAB/data/MERRA2/Monthly/M2TMNXSLV.5.12.4/2012/MERRA2_400.tavgM_2d_slv_Nx.201212.nc4']
+        dateRange = pandas.date_range('2010-11-11', '2011-01-12')
+        outDir = tempfile.mkdtemp()
+        print 'od = ' + str(outDir)
+
+        MerraRequest.run(env, dateRange, MerraRequest.MONTHLY, ['m2t1nxslv'],
+                         ['avg'], outDir)
                          
-        self.assertEqual(expectedFiles, files)
+        # Delete the clipped files.
         
