3ac59f67f8b9a1e1191873805b4e2871afac750a
Savannah Strong Finch <69205623+ssfinch@users.noreply.github.com>
2021-07-16 14:15:55 -0400

    Create Hello_World.py
    Stored a simple Hello World script here so that I could build a container and install the code into a container for a demo for Mark.

---
+++ demo/Hello_World.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+Created on Oct 4, 2018
+
+@author: sstrong
+'''
+import time
+import sys
+
+class UserApp(object):
+
+    if __name__ == '__main__':
+
+         start_time = time.time()
+         elapsed_time = time.time() - start_time
+         print (elapsed_time, " seconds wall time for Hello World script\n")
